1b727a9bba35389e3d26ff49b5ccd43b4110ca58
Alinka08 <alinazoldubaeva@gmail.com>
2023-06-09 22:00:54 -0500

    car's cards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import "./App.css";
 import StudentCard from "./components/card/Card";
-import CarsList from "./components/studentsList-demo/studentsList-demo";
-import StudentsListdemo from "./components/studentsList-demo/studentsList-demo";
+import CarsList from "./components/carsList/CarsList";
+
 import StudentsList from "./components/studentsList/StudentsList";
-import TodoAppdemo from "./todoApp-demo/TodoAppdemo";
+
 import TodoApp from "./todoApp/TodoApp";
 
 function App() {
   return (
     <div className="App">
       {/* <TodoApp /> */}
-      {/* <TodoAppdemo /> */}
-      {/* <TodoAppdemo/> */}
       {/* <StudentsList /> */}
       <CarsList />
     </div>
